e5402dcbc0c6b80ca86d3db43865c5398a5138fe
tddebart <tddebart@gmail.com>
2021-10-12 22:56:16 +0200

    Initial commit

---
+++ js/intro.js
@@ -0,0 +1,43 @@
+let header = document.querySelector('#intro');
+let skip = true;
+let frames = [
+    { t: "{ }", ms: 200 },
+    { t: "{_}", ms: 200 },
+    { t: "{ }", ms: 200 },
+    { t: "{_}", ms: 200 },
+    { t: "{T_}", ms: 100 },
+    { t: "{TD_}", ms: 100 },
+    { t: "{TDD_}", ms: 100 },
+    { t: "{TDDE_}", ms: 100 },
+    { t: "{TDDEB_}", ms: 100 },
+    { t: "{TDDEBA_}", ms: 100 },
+    { t: "{TDDEBAR_}", ms: 100 },
+    { t: "{TDDEBART_}", ms: 100 },
+    { t: "{TDDEBART }", ms: 200 },
+    { t: "{TDDEBART_}", ms: 200 },
+    { t: "{TDDEBART }", ms: 200 },
+    { t: "{TDDEBART_}", ms: 200 },
+    { t: "{TDDEBART}", ms: 200 },
+    { t: "{TDDEBART}", ms: 200 }
+];
+let stepDenominator = 1;
+if (window.localStorage.stepDenominator)
+    stepDenominator = window.localStorage.stepDenominator;
+let i = 0;
+let update = () => {
+    let step = frames[i];
+    header.innerText = step.t;
+    i++;
+
+    if (i < frames.length && !skip)
+        setTimeout(update, step.ms / stepDenominator);
+    else {
+        header.classList.add('top');
+        setTimeout(() => {
+            document.getElementById('main').style.opacity = 1;
+            initGlobe();
+        }, 500);
+        window.localStorage.stepDenominator = 2;
+    }
+}
+update();
---
+++ js/project_data.js
@@ -0,0 +1,25 @@
+const ProjectData = {
+    "rounds_mods": {
+        title: "Rounds mods",
+        github: "https://github.com/tddebart/BossSlothsMods",
+        description: ""
+    },
+    "unity": {
+        title: "Unity",
+        github: "",
+        description: ""
+    },
+    "mastermind": {
+        title: "Mastermind",
+        github: "https://github.com/tddebart/Mastermind/",
+        description: ""
+    },
+
+
+
+    "more": {
+        title: "More",
+        github: "https://github.com/tddebart?tab=repositories&q=&type=&language=python&sort=",
+        description: "Find more of my python projects on my github"
+    }
+}
