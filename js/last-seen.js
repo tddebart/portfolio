329ba001f29f3b41349ec9ba8d85e47f1f911d27
tddebart <tddebart@gmail.com>
2021-10-13 15:20:49 +0200

    removed unused code

--- js/last-seen.js
+++ js/last-seen.js
@@ -60,21 +60,6 @@ function updateSeen() {
             seenEl.innerHTML = "I'm not working on anything right now."
         }
     })
-
-    // fetch('https://api.github.com/users/tddebart/repos').then(r => r.text()).then(r => {
-    //
-    //     let json = JSON.parse(r);
-    //     if (json.project) {
-    //         if (json.project_href)
-    //             // Please no XSS
-    //             seenEl.innerHTML = `I was last seen working on <a href="${json.project_href}" class="d" target="_blank"
-    //             style="color:#949494;">${json.project}<span class="underline" style="background-color:#949494;"></span></a>.`;
-    //         else
-    //             seenEl.innerText = 'I was last seen working on ' + json.project + '.';
-    //     } else {
-    //         seenEl.innerText = "I'm not working on anything right now."
-    //     }
-    // }).catch(console.log);
 }
 
 setInterval(updateSeen, 60 * 1000);
