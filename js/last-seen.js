811033f76c41ef3f2f1daa3030c4f0d6aefc04aa
tddebart <tddebart@gmail.com>
2021-10-13 14:15:59 +0200

    last seen

--- js/last-seen.js
+++ js/last-seen.js
@@ -1,7 +1,16 @@
 
 const seenEl = document.querySelector('#lastseen');
 
+String.prototype.format = function() {
+    a = this;
+    for (k in arguments) {
+        a = a.replace("{" + k + "}", arguments[k])
+    }
+    return a
+}
+
 function updateSeen() {
+    let recentRepo;
     let lowestTime = 0;
     fetch('https://api.github.com/users/tddebart/repos', {
         headers: {
@@ -10,13 +19,18 @@ function updateSeen() {
         }}).then(r => r.text()).then(r => {
         let loc = JSON.parse(r);
         for (const l of loc) {
-            console.log(new Date(l.updated_at).getTime() + " | " + (new Date(l.updated_at).getTime() > lowestTime))
             if(new Date(l.updated_at).getTime() > lowestTime) {
                 lowestTime = new Date(l.updated_at).getTime()
+                recentRepo = l;
             }
 
         }
-        console.log(lowestTime)
+        if(recentRepo !== undefined) {
+            seenEl.innerHTML = 'I was last seen working on <a href="{0}" class="d" target="_blank"\n'.format(recentRepo.html_url) +
+                'style="color:#949494;">{0}<span class="underline" style="background-color:#949494;"></span></a>'.format(recentRepo.name)
+        } else {
+            seenEl.innerHTML = "I'm not working on anything right now."
+        }
     })
 
     // fetch('https://api.github.com/users/tddebart/repos').then(r => r.text()).then(r => {
